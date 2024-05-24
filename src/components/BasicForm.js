c17e1f4912fe8f5c4c80fe7b5f1efbfe37000e94
anouar garbaa <g.anouar@yahoo.com>
2024-05-24 13:43:17 +0100

    add of use formik components

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,17 +1,34 @@
+// *  USE FORMIK HOOK *
+
 import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
+
+// clear all the fields on click submit button
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
 
 const BasicForm = () => {
-  const {values, handleBlur, handleChange} = useFormik({
+  const {
+    values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit,
+  } = useFormik({
     initialValues: {
       email: "",
       age: "",
       password: "",
       confirmPassword: "",
     },
+    validationSchema: basicSchema,
+    onSubmit,
   });
 
+  console.log(errors);
+
   return (
-    <form autoComplete="off">
+    <form onSubmit={handleSubmit} autoComplete="off">
       <label htmlFor="email">Email</label>
       <input
         value={values.email}
@@ -19,38 +36,52 @@ const BasicForm = () => {
         id="email"
         type="email"
         placeholder="Enter your email"
+        onBlur={handleBlur}
+        className={errors.email && touched.email ? "input-error" : ""}
       />
-
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
       <label htmlFor="age">Age</label>
-       <input
-         id="age"
-         type="number"
-         placeholder="Enter your age"
-         value={values.age}
-         onChange={handleChange}
-         onBlur={handleBlur}
-       />
-
-       <label htmlFor="password">Password</label>
-       <input
-         id="password"
-         type="password"
-         placeholder="Enter your password"
-         value={values.password}
-         onChange={handleChange}
-         onBlur={handleBlur}
-       />
-
-       <label htmlFor="confirmPassword">Confirm Password</label>
-       <input
-         id="confirmPassword"
-         type="password"
-         placeholder="Please Confirm your password"
-         value={values.confirmPassword}
-         onChange={handleChange}
-         onBlur={handleBlur}
-       />
-       <button type="submit">Submit</button>
+      <input
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        value={values.age}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.age && touched.age ? "input-error" : ""}
+      />
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        placeholder="Enter your password"
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.password && touched.password ? "input-error" : ""}
+      />
+      {errors.password && touched.password && (
+        <p className="error">{errors.password}</p>
+      )}
+      <label htmlFor="confirmPassword">Confirm Password</label>
+      <input
+        id="confirmPassword"
+        type="password"
+        placeholder="Confirm password"
+        value={values.confirmPassword}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={
+          errors.confirmPassword && touched.confirmPassword ? "input-error" : ""
+        }
+      />
+      {errors.confirmPassword && touched.confirmPassword && (
+        <p className="error">{errors.confirmPassword}</p>
+      )}
+      <button disabled={isSubmitting} type="submit">
+        Submit
+      </button>
     </form>
   );
 };
